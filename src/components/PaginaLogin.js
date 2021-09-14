06f98fc928e879fa8f04fd9508f8c3ab22487cc9
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-09-14 11:41:59 -0300

    Agregada pagina para loguear con formulario submit

---
+++ src/components/PaginaLogin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormularioSuscripcion from './FormularioSuscripcion';
+
+const PaginaLogin = () => {
+    return (
+        <div>
+            <h1>Aplicación Superheroes</h1>
+            <p>Logueate para ingresar</p>
+            <FormularioSuscripcion />
+        </div>
+    )
+}
+
+export default PaginaLogin
